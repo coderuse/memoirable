92e604bfa4f56739705d24591741b2d69855ee44
Arnab Das <arnab@coderuse.com>
2016-11-14 18:12:31 +0530

    Configured for hosting in custom domain

--- src/app/browserHistory.ts
+++ src/app/browserHistory.ts
@@ -4,7 +4,7 @@ import { createHistory } from 'history';
 import { Router, Route, IndexRoute, Link, useRouterHistory } from 'react-router';
 
 const browserHistory = useRouterHistory(createHistory)({
-  basename: '/memoirable'
+  basename: '/'
 });
 
 export default browserHistory;
