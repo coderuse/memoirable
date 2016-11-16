7b809aefbc08bc85c8c022710103f246dea60366
Chandan Kumar Jha <chandan1909@gmail.com>
2016-11-16 11:36:32 +0530

    Calendar based on CSS percentages (#16)
    * Used Marked as the markdown library
    
    * some css changes
    
    * using react markdown
    
    * Initial commit for ace editor
    
    * Added the ace editor
    
    * prettify code
    
    * google drive initial saving
    
    * Scss for header on dashboard
    
    * Set initial structure of memoirable app in google drive
    
    * Calendar Integration
    
    * Finalizing the Calendar component
    
    * Finished Calendar Component
    
    * Made percentage based values in css for calendar

--- src/app/views/home.tsx
+++ src/app/views/home.tsx
@@ -2,11 +2,10 @@
 
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import browserHistory from '../browserHistory';
 import { AuthActionTypes, ProviderTypes } from '../actions/types';
 import * as AuthActions from '../actions/authActions';
-import GAuthStore from '../stores/gAuthStore';
-
-import browserHistory from '../browserHistory';
+import GAuthStore from '../stores/gAuthStore'; 
 
 export class Home extends React.Component<{}, {}> {
   _listenerToken: FBEmitter.EventSubscription;
