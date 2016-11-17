95b90993ceb1356e3e74f17d105c7c22c8e88d2a
Arnab Das <arnab@coderuse.com>
2016-11-17 13:03:57 +0530

    Check browser version view

--- src/app/views/checkBrowserVersion.tsx
+++ src/app/views/checkBrowserVersion.tsx
@@ -4,11 +4,24 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import browserHistory from '../browserHistory';
 
-export class Home extends React.Component<{}, {}> {
+export class CheckBrowserVersion extends React.Component<{}, {}> {
+  _isChrome: boolean;
   constructor() {
     super();
     
-    const isChrome = !!window['chrome'] && !!window['chrome']['webstore'];
+    this._isChrome = !!window['chrome'] && !!window['chrome']['webstore'];  
   }
 
+  componentWillMount() {
+    if (this._isChrome)
+      browserHistory.push('/home');
+  }
+
+  render() {
+    return (
+      <div className="incompatible-browser-notice">
+        Currently we only support Google Chrome. Please open this site in same.
+      </div>
+    );
+  }
 }
